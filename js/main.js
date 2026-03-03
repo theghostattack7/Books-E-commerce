e8be46c9c57b5c41936dfc3206fd3e4aab2db17d
Fernando Resende <theghostattack7@gmail.com>
2026-03-03 12:56:04 +0100

    trying to incorporate javascript

--- js/main.js
+++ js/main.js
@@ -17,3 +17,46 @@ function clickFunction(event) {
   localStorage.number++;
   cartCount.innerText = localStorage.number;
 }
+
+const twilightButton = document.getElementById("twilightButton");
+const draculaButton = document.getElementById("draculaButton");
+const duneButton = document.getElementById("duneButton");
+const odysseyButton = document.getElementById("odysseyButton");
+const steveJobsButton = document.getElementById("steveJobsButton");
+const peopleWeMeetOnVacationButton = document.getElementById(
+  "peopleWeMeetOnVacationButton",
+);
+const hungerGamesButton = document.getElementById("hungerGamesButton");
+
+const lotrButton = document.querySelector(".lordOfTheRingsButton");
+
+if (lotrButton) {
+  lotrButton.addEventListener("click", addToCart);
+}
+
+function addToCart() {
+  localStorage.setItem(
+    "cartItem",
+    "Lord of the Rings -------------------------------  $14.39",
+  );
+  localStorage.setItem("price", "14.39");
+}
+
+const productsInCart = document.querySelector(".products-in-cart");
+const priceTotal = document.querySelector(".price-total");
+
+if (productsInCart) {
+  const item = localStorage.getItem("cartItem");
+
+  if (item) {
+    productsInCart.innerText = item;
+  }
+}
+
+if (priceTotal) {
+  const cartPrice = localStorage.getItem("price");
+
+  if (cartPrice) {
+    priceTotal.innerText = item;
+  }
+}
